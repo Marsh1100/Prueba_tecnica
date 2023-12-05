8ce671bce1f5fb4d1d2446d38e298f3ce7236c2f
Marsh1100 <margie.b.agu17@gmail>
2023-12-04 19:55:34 -0500

    feat: :lipstick: Add loop

--- reto2/Program.cs
+++ reto2/Program.cs
@@ -2,21 +2,33 @@
 //Multiplicacion Rusa
 //Desarrolle un programa que reciba como entrada el multiplicador y el multiplicando, y entregue como resultado el producto de ambos, calculado mediante el método de multiplicación rusa.
 
-Console.Write("Ingrese el número del multiplicador: ");
-int m1 = Int32.Parse(Console.ReadLine());
-Console.Write("Ingrese el número del multiplicando:");
-int m2 = Int32.Parse(Console.ReadLine());
-
-int suma = 0;
-while (m1 != 0)
+string seguir="Y";
+do
 {
-    if (m1%2 != 0)
-    {        
-        suma +=m2;
-        m2 *=2;
-    }else{
-        m2 *=2;
+    Console.ForegroundColor=ConsoleColor.Magenta; 
+    Console.WriteLine("\n------------------------------------- MULTIPLICACIÓN RUSA --------------------------------------");
+    Console.ResetColor();
+    Console.Write("Ingrese el número del multiplicador: ");
+    int m1 = Int32.Parse(Console.ReadLine());
+    Console.Write("Ingrese el número del multiplicando:");
+    int m2 = Int32.Parse(Console.ReadLine());
+
+    int suma = 0;
+    while (m1 != 0)
+    {
+        if (m1%2 != 0)
+        {        
+            suma +=m2;
+            m2 *=2;
+        }else{
+            m2 *=2;
+        }
+        m1 /=2;
     }
-    m1 /=2;
-}
-Console.WriteLine($"Resultado: {suma}");
+    Console.WriteLine($"Resultado: {suma}");
+
+
+    Console.Write("\n¿Desea agregar nuevos valores? (Y/N): ");
+    seguir = Console.ReadLine();
+}while(seguir.ToUpper()!="N");
+
