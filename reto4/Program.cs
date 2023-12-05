04e87664258bdee3d1d8c18cc29924832886212e
Marsh1100 <margie.b.agu17@gmail>
2023-12-04 20:09:47 -0500

    feat: :lipstick: Visualización terminal

--- reto4/Program.cs
+++ reto4/Program.cs
@@ -1,7 +1,10 @@
 ﻿// Votaciones de la CONFECH
 
 Dictionary<string, List<string>> votosUniversidades = new(); 
-Console.WriteLine("\nVotaciones de la CONFECH");
+Console.ForegroundColor=ConsoleColor.Magenta; 
+Console.WriteLine("\n---------------- Votaciones de la CONFECH ------------------------");
+Console.ResetColor();
+
 
 Console.Write("Ingrese número de universidades que participan en el proceso: ");
 int n = Int32.Parse(Console.ReadLine());
@@ -15,29 +18,38 @@ for (var i= 1; i <= n; i++)
     
     List<string> votos = new();
     string voto = "A";
-    while(voto!="X")
+    do
     {
         Console.Write($"Voto: ");
         voto = Console.ReadLine(); 
 
         if(voto.ToUpper()!="A" && voto.ToUpper()!="R" && voto.ToUpper()!="N" && voto.ToUpper()!="B"  && voto.ToUpper()!="X")
         {
-            Console.Write("Las opciones de marcado es inválida");
+            Console.ForegroundColor=ConsoleColor.Red; 
+            Console.WriteLine("La opción de marcado es inválida");
+            Console.ResetColor();
+
         }else
         {
-            votos.Add(voto);
+            votos.Add(voto.ToUpper());
         }
-    }
+    }while(voto.ToUpper()!="X");
 
     votosUniversidades.Add(nombreUniversidad, votos);
 };
 
 List<int> resultados = new(){0,0,0};
 
+Console.ForegroundColor=ConsoleColor.Cyan; 
+Console.WriteLine("\n---------------- RESULTADOS ------------------------");
+Console.ResetColor();
 foreach(var uni in votosUniversidades)
 {
     List<int> votos = new();
+    Console.ForegroundColor=ConsoleColor.Yellow; 
     Console.WriteLine($"Universidad: {uni.Key}");
+    Console.ResetColor();
+
     foreach(var v in uni.Value)
     {
         Console.WriteLine($"Voto:{v}");
@@ -51,7 +63,9 @@ foreach(var uni in votosUniversidades)
     //Nulo
     votos.Add(uni.Value.Where(a=> a=="N").Count());  
 
+    Console.ForegroundColor=ConsoleColor; 
     Console.WriteLine($"{votos[0]} aceptan, {votos[1]} rechazan, {votos[2]} blancos, {votos[3]} nulos.\n");  
+    Console.ResetColor();
 
     if(votos[0]> votos[1])
     {
@@ -59,11 +73,12 @@ foreach(var uni in votosUniversidades)
     }else if(votos[0]< votos[1])
     {
         resultados[1] += 1;
-    }else
+    }else if(votos[0]!=0)
     {
         resultados[2] += 1;
     }
 }
+
 
 Console.WriteLine($"Universidades que aceptan: "+resultados[0]);
 Console.WriteLine($"Universidades que rechazan: "+resultados[1]);
